4a3df0a1fb9fa1dda4de6aa18e75704a3cb622a6
pycbouh <pycbouh@users.noreply.github.com>
2021-08-22 14:05:34 +0000

    Deploying to gh-pages from  @ 43a078203ecb9215c03255dccb019b7d5fea9a6d 🚀

---
+++ scripts/shared/global.js
@@ -0,0 +1,73 @@
+(function () {
+  'use strict';
+
+  // API Interaction
+  const ReportsAPI = {
+    async get(path = '/') {
+      const res = await fetch(`${path}`);
+      if (res.status !== 200) {
+        return null;
+      }
+
+      return await res.json();
+    },
+
+    async getData() {
+      return await this.get("data.json");
+    },
+  };
+
+  // Content helpers
+  const ReportsFormatter = {
+    formatDate(dateString) {
+      const options = {
+        year: 'numeric', month: 'long', day: 'numeric'
+      };
+      const dateFormatter =  new Intl.DateTimeFormat('en-US', options);
+
+      const date = new Date(dateString);
+      return dateFormatter.format(date);
+    },
+
+    formatTimestamp(timeString) {
+      const options = {
+        year: 'numeric', month: 'long', day: 'numeric',
+        hour: 'numeric', hour12: false, minute: 'numeric',
+        timeZone: 'UTC', timeZoneName: 'short'
+      };
+      const dateFormatter =  new Intl.DateTimeFormat('en-US', options);
+
+      const date = new Date(timeString);
+      return dateFormatter.format(date);
+    },
+
+    getDaysSince(dateString) {
+      const date = new Date(dateString);
+      const msBetween = (new Date()) - date;
+      const days = Math.floor(msBetween / (1000 * 60 * 60 * 24));
+
+      return days;
+    },
+
+    formatDays(days) {
+      return days + " " + (days !== 0 ? "days" : "day")
+    },
+  };
+
+  const ReportsUtils = {
+    createEvent(name, detail = {}) {
+      return  new CustomEvent(name, {
+        detail: detail
+      });
+    },
+  };
+
+  const ReportsSingleton = {
+    api: ReportsAPI,
+    format: ReportsFormatter,
+    util: ReportsUtils,
+  };
+
+  window.greports = ReportsSingleton;
+
+}());
